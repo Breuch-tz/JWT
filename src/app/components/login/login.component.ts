7a6efc6165ffbebdbf74748248e05598c497e6e4
Tizian Breuch <Tizian.Breuch@edvring.de>
2023-04-18 15:56:28 +0200

    abschluss

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,32 +6,40 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  form:FormGroup;
+  form: FormGroup;
 
-  constructor(private fb:FormBuilder, 
-               private authService: AuthService, 
-               private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
-      this.form = this.fb.group({
-          email: ['',Validators.required],
-          password: ['',Validators.required]
+  public async submit() {
+    await this.login();
+  }
+
+  public async login() {
+    const val = this.form.value;
+
+    if (val.email && val.password) {
+      this.authService.login(val.email, val.password).subscribe((res) => {  //warum obs und nicht promise
+        
+        const token =  res;
+        document.cookie = `token=${token}`
+
+        console.log('User is logged in');
+        this.router.navigateByUrl('/dashboard');
       });
+    }
   }
 
-  login() {
-      const val = this.form.value;
-
-      if (val.email && val.password) {
-          this.authService.login(val.email, val.password)
-              .subscribe(
-                  () => {
-                      console.log("User is logged in");
-                      this.router.navigateByUrl('/dashboard');
-                  }
-              );
-      }
-  }
+  
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,36 +9,47 @@ import * as moment from 'moment';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  login(email: string, password: string) {
-    return this.http.post<any>('http://localhost:3001/api/login', { email, password }).pipe(
+  
+
+
+  login(username: string, passwort: string) {
+    return this.http.post<any>('http://localhost:3001/api/createNewUser',  {username, passwort} ).pipe(
       // this is just the HTTP call,
       // we still need to handle the reception of the token
       shareReplay()
+
     );
   }
-  private setSession(authResult: any) {
-    const expiresAt = moment().add(authResult.expiresIn, 'second');
+  
 
-    localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
-  }
 
-  logout() {
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('expires_at');
-  }
+  
 
-  public isLoggedIn() {
-    return moment().isBefore(this.getExpiration());
-  }
 
-  isLoggedOut() {
-    return !this.isLoggedIn();
-  }
 
-  getExpiration() {
-    const expiration = localStorage.getItem('expires_at');
-    const expiresAt = JSON.parse(expiration!);
-    return moment(expiresAt);
-  }
+  // private setSession(authResult: any) {
+  //   const expiresAt = moment().add(authResult.expiresIn, 'second');
+
+  //   localStorage.setItem('id_token', authResult.idToken);
+  //   localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+  // }
+
+  // logout() {
+  //   localStorage.removeItem('id_token');
+  //   localStorage.removeItem('expires_at');
+  // }
+
+  // public isLoggedIn() {
+  //   return moment().isBefore(this.getExpiration());
+  // }
+
+  // isLoggedOut() {
+  //   return !this.isLoggedIn();
+  // }
+
+  // getExpiration() {
+  //   const expiration = localStorage.getItem('expires_at');
+  //   const expiresAt = JSON.parse(expiration!);
+  //   return moment(expiresAt);
+  // }
 }
